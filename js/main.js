356912910e38f1eae134e1d76f0a3ea8cd80c625
abhishekg785 <abhishekg785@gmail.com>
2017-01-29 20:31:35 +0530

    bricks added

--- js/main.js
+++ js/main.js
@@ -26,10 +26,33 @@ GameSimulation = {}; exports = GameSimulation;
         leftPressed = false,
         rightPressed = false;
 
+        /* parameters for the bricks comes here */
+        var brickParams = {
+            brickRowCount : 3,
+            brickColumnCount : 5,
+            brickWidth : 75,
+            brickHeight : 20,
+            brickPadding : 10,
+            brickOffsetTop : 30,
+            brickOffsetLeft : 30
+        }
+        var bricks = [];
+        for(var c = 0 ; c < brickParams.brickColumnCount; c++ ) {
+            bricks[c] = [];
+            for(var r = 0; r < brickParams.brickRowCount; r++ ) {
+                bricks[c][r] = {
+                    x : 0,
+                    y : 0
+                }
+            }
+        }
+
     _Functions = {
 
         /**
          * Draw the ball on the game canvas
+         *
+         * @return {{drawBall: _Functions.drawBall, drawPaddle: _Functions.drawPaddle, addBounds: _Functions.addBounds, draw: _Functions.draw, keyDownHandler: _Functions.keyDownHandler, keyUpHandler: _Functions.keyUpHandler}|*}
          */
         drawBall : function() {
 
@@ -42,6 +65,11 @@ GameSimulation = {}; exports = GameSimulation;
             return _Functions;
         },
 
+        /**
+         * Draw the paddle on the canvas
+         *
+         * @return {{drawBall: _Functions.drawBall, drawPaddle: _Functions.drawPaddle, addBounds: _Functions.addBounds, draw: _Functions.draw, keyDownHandler: _Functions.keyDownHandler, keyUpHandler: _Functions.keyUpHandler}|*}
+         */
         drawPaddle : function() {
 
             ctx.beginPath();
@@ -54,13 +82,50 @@ GameSimulation = {}; exports = GameSimulation;
         },
 
         /**
+         * Draw Bricks on the canvas
+         *
+         * @return {{drawBall: _Functions.drawBall, drawPaddle: _Functions.drawPaddle, drawBricks: _Functions.drawBricks, addBounds: _Functions.addBounds, draw: _Functions.draw, keyDownHandler: _Functions.keyDownHandler, keyUpHandler: _Functions.keyUpHandler}|*}
+         */
+        drawBricks : function() {
+            for(c = 0 ; c < brickParams.brickColumnCount; c++) {
+                for(r = 0; r < brickParams.brickRowCount; r++) {
+                    brickX = (c * (brickParams.brickWidth + brickParams.brickPadding)) + brickParams.brickOffsetLeft;
+                    brickY = (r * (brickParams.brickHeight + brickParams.brickPadding)) + brickParams.brickOffsetTop;
+                    bricks[c][r].x = brickX;
+                    bricks[c][r].y = brickY;
+                    ctx.beginPath();
+                    ctx.rect(brickX, brickY, brickParams.brickWidth, brickParams.brickHeight);
+                    ctx.fillStyle = '#0095DD';
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+            return _Functions;
+        },
+
+        /**
          * Add bounds to the moving ball so that it remains inside the game canvas
          * Reverses the dx and dy factors
+         *
+         * @return {{drawBall: _Functions.drawBall, drawPaddle: _Functions.drawPaddle, addBounds: _Functions.addBounds, draw: _Functions.draw, keyDownHandler: _Functions.keyDownHandler, keyUpHandler: _Functions.keyUpHandler}|*}
          */
         addBounds : function() {
 
-            if(y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+            if(y + dy < ballRadius) {
                 dy = -dy;
+            }
+            else if(y + dy > canvas.height - ballRadius) {
+                /*
+                 * collision detection b/w the ball and the paddle
+                 * check whether the center of the ball is b/w the left and the right edges of the paddle
+                 */
+                if(x > paddleX && x < paddleX + paddleWidth) {
+                    dy = -dy;
+                }
+                else {
+                    alert("GAME OVER!");
+                    document.location.reload();
+                }
             }
             if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
                 dx = -dx;
@@ -76,12 +141,9 @@ GameSimulation = {}; exports = GameSimulation;
         draw : function() {
 
             window.requestAnimationFrame(_Functions.draw);
+            /* clear the canvas with each frame */
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            _Functions.addBounds().drawBall().drawPaddle();
-
-            /* Ball movements */
-            x += dx;
-            y += dy;
+            _Functions.drawBall().drawPaddle().drawBricks().addBounds();
 
             /* Paddle movements */
             if(rightPressed && paddleX < canvas.width - paddleWidth) {
@@ -90,6 +152,10 @@ GameSimulation = {}; exports = GameSimulation;
             else if(leftPressed && paddleX > 0) {
                 paddleX -= 7;
             }
+
+            /* Ball movements */
+            x += dx;
+            y += dy;
         },
 
         keyDownHandler : function(e) {
@@ -112,6 +178,19 @@ GameSimulation = {}; exports = GameSimulation;
                 leftPressed = false;
             }
 
+        },
+
+        /**
+         * Simply detects the collision b/w the brick and the ball
+         * Not much effective but will work for the time
+         */
+        collisionDetection : function() {
+
+            for(c = 0; c < brickParams.brickColumnCount; c++) {
+                for(r = 0; r < brickParams.brickRowCount; r++) {
+                    var b = bricks[c][r];
+                }
+            }
         }
     }
 
