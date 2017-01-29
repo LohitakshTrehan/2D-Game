899e579d258246a0f2143c1c30e3fc32ecb4bb91
abhishekg785 <abhishekg785@gmail.com>
2017-01-29 19:30:40 +0530

    ball on the canvas created and bounds added to the canvas

---
+++ README.md
@@ -0,0 +1,13 @@
+#2D Game using pure JS and HTML5 < canvas > 
+
+Starting with pure JS is the best way to get a solid knowledge about web game development.<br/>
+After that you can pick any of the frameworks you like and use it in your projects.
+
+Even the knowledge on how the framework works is important as you are going to use them.<br/>
+What if someone has done some malicious activity in the framework's code :P poor joke! <br/>
+
+All i want is to understand that even how the frameworks work!
+
+It will be a simple game where user has to break the bars using a ball and the ball should land on the moving platform otherwise you loose a life, 
+a simple 90's game we used to play.
+
---
+++ js/main.js
@@ -0,0 +1,68 @@
+GameSimulation = {}; exports = GameSimulation;
+
+(function(d, w) {
+
+    window.requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        function callback() {
+            window.setTimeout(callback, 1000 / 60);
+        }
+
+    var canvas = document.getElementById('gameCanvas'),
+        ctx = canvas.getContext('2d'),
+        ballRadius = 10,
+        x = canvas.width / 2,
+        y = canvas.height - 30,
+        dx = 2,
+        dy = -2;
+
+    _Functions = {
+
+        /**
+         * Draw the ball on the game canvas
+         */
+        drawBall : function() {
+
+            ctx.beginPath();
+            ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+            ctx.fillStyle = '#0095DD';
+            ctx.fill();
+            ctx.closePath();
+
+        },
+
+        /**
+         * Add bounds to the moving ball so that it remains inside the game canvas
+         * Reverses the dx and dy factors
+         */
+        addBounds : function() {
+
+            if(y + dy < 0 || y + dy > canvas.height) {
+                dy = -dy;
+            }
+            if(x + dx > canvas.width || x + dx < 0) {
+                dx = -dx;
+            }
+
+        },
+
+
+        /**
+         * Draw on the game canvas every 10 ms
+         */
+        draw : function() {
+
+            window.requestAnimationFrame(_Functions.draw);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            _Functions.addBounds();
+            _Functions.drawBall();
+            x += dx;
+            y += dy;
+        }
+    }
+
+    _Functions.draw();
+
+})(document, window);
